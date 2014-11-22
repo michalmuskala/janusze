2a7410d38293ffc97445809bfbe764e0d177a457
Michal Muskala <michal@muskala.eu>
2014-11-22 12:53:37 +0100

    Use s3 for files

--- Gemfile
+++ Gemfile
@@ -45,6 +45,7 @@ end
 
 group :production do
   gem 'rails_12factor'
+  gem 'fog'
 end
 
 gem 'traceur-rails'
---
+++ config/initializers/carrierwave.rb
@@ -0,0 +1,17 @@
+CarrierWave.configure do |config|
+  config.fog_credentials = {
+    :provider               => 'AWS',
+    :aws_access_key_id      => ENV['AWS_ACCESS_KEY'],
+    :aws_secret_access_key  => ENV['AWS_SECRET'],
+    :region                 => 'eu-central-1'
+  }
+  config.fog_directory  = 'janusz3'
+  config.fog_public     = true
+  config.fog_attributes = {'Cache-Control'=>"max-age=#{365.day.to_i}"}
+
+  if Rails.env.production?
+    config.storage :fog
+  else
+    config.storage :file
+  end
+end
